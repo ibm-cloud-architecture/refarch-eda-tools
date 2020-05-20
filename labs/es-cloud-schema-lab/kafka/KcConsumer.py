8371a3c2ae8e3b5ecdfebc597c1d8a8937739fec
Jesus Almaraz <almarazj@ie.ibm.com>
2020-05-20 19:27:24 +0200

    es on cloud schema registry lab

---
+++ labs/es-cloud-schema-lab/kafka/KcConsumer.py
@@ -0,0 +1,58 @@
+import json,os
+from confluent_kafka import Consumer, KafkaError
+
+
+class KafkaConsumer:
+
+    def __init__(self, kafka_brokers = "", kafka_apikey = "", topic_name = "",autocommit = True):
+        self.kafka_brokers = kafka_brokers
+        self.kafka_apikey = kafka_apikey
+        self.topic_name = topic_name
+        self.kafka_auto_commit = autocommit
+
+    # See https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
+    # Prepares de Consumer with specific options based on the case
+    def prepareConsumer(self, groupID = "ConsumePlainMessagePython"):
+        options ={
+                'bootstrap.servers':  self.kafka_brokers,
+                'group.id': groupID,
+                'auto.offset.reset': 'earliest',
+                'enable.auto.commit': self.kafka_auto_commit,
+                'security.protocol': 'SASL_SSL',
+                'sasl.mechanisms': 'PLAIN',
+                'sasl.username': 'token',
+                'sasl.password': self.kafka_apikey
+        }
+        # Print the configuration
+        print("--- This is the configuration for the consumer: ---")
+        print(options)
+        print("---------------------------------------------------")
+        # Create the consumer
+        self.consumer = Consumer(options)
+        # Subscribe to the topic
+        self.consumer.subscribe([self.topic_name])
+    
+    # Prints out and returns the decoded events received by the consumer
+    def traceResponse(self, msg):
+        msgStr = msg.value().decode('utf-8')
+        print('[Message] - Next Message consumed from {} partition: [{}] at offset {} with key {}:\n\tmessage: {}'
+                    .format(msg.topic(), msg.partition(), msg.offset(), str(msg.key()), msgStr ))
+
+    # Polls for next event
+    def pollNextEvent(self):
+        # Poll for messages
+        msg = self.consumer.poll(timeout=10.0)
+        # Validate the returned message
+        if msg is None:
+            print("[INFO] - No new messages on the topic")
+        elif msg.error():
+            if ("PARTITION_EOF" in msg.error()):
+                print("[INFO] - End of partition")
+            else:
+                print("[ERROR] - Consumer error: {}".format(msg.error()))
+        else:
+            # Print the message
+            msgStr = self.traceResponse(msg)
+    
+    def close(self):
+        self.consumer.close()
